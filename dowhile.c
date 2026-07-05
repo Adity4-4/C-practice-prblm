3d2bb3599335a8b45d5f5d640df1e0cea6f7ada0
Aditya <adityawatekar835@gmail.com>
2026-07-05 09:06:54 +0530

    do while  problems

--- dowhile.c
+++ dowhile.c
@@ -13,3 +13,67 @@ int main(){
         return 0;
     
 }
+
+#include <stdio.h>
+
+int main(){
+    int i=0;
+    do{
+        i=i+1;
+        if(i%2==1){
+            printf("%d \n",i);
+        
+        }
+
+    }
+    while(i<15);
+    return 0;
+}
+
+int main(){
+    int i=0,n,m,l;
+
+    printf("Enter the number n \n");
+    scanf("%d",&n);
+    printf("Enter the numbers \n");
+    scanf("%d",&m);
+    
+    do{
+        scanf("%d",&l);
+        if(l>m){
+            m=l;
+        }
+      
+i=i+1;
+    }
+
+    while(i<n);
+     printf("Largest number is %d",m);
+
+    return 0;
+     
+}
+
+int main(){
+    int countm=0,gender,f,countf=0;
+    printf("Enteries in office for Male is 1 and female is 2 \n");
+    do{
+        scanf("%d",&gender);
+        if(gender==1){
+            countm=countm+1;
+        }
+        else if(gender==2){
+            countf=countf+1;
+        }
+        else if(gender==3){
+            break;
+        }
+        else{
+            printf("invalid input \n");
+        }
+        
+    }
+    while(0<1);
+     printf("count of Male is %d: \n",countm );
+     printf("count of Female is %d: ",countf);
+}
