c673dcf662c5ff82daf9321dac4f5a04ab70345e
Aditya <adityawatekar835@gmail.com>
2026-07-05 09:11:36 +0530

    do while  problems

--- dowhile.c
+++ dowhile.c
@@ -1,79 +1,92 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int main(){
-    int i=0,l,w,h,v;
-    do{
-        printf("\n Enter l,w,h: \n");
-        scanf("%d %d %d",&l, &h, &w);
-        v=l*w*h;
-        printf("\n The Volume is %d",v);
-        i=i+1;
-    }
-    while(i<2);
-        return 0;
+// int main(){
+//     int i=0,l,w,h,v;
+//     do{
+//         printf("\n Enter l,w,h: \n");
+//         scanf("%d %d %d",&l, &h, &w);
+//         v=l*w*h;
+//         printf("\n The Volume is %d",v);
+//         i=i+1;
+//     }
+//     while(i<2);
+//         return 0;
     
-}
+// }
+
+// #include <stdio.h>
+
+// int main(){
+//     int i=0;
+//     do{
+//         i=i+1;
+//         if(i%2==1){
+//             printf("%d \n",i);
+        
+//         }
+
+//     }
+//     while(i<15);
+//     return 0;
+// }
+
+// int main(){
+//     int i=0,n,m,l;
+
+//     printf("Enter the number n \n");
+//     scanf("%d",&n);
+//     printf("Enter the numbers \n");
+//     scanf("%d",&m);
+    
+//     do{
+//         scanf("%d",&l);
+//         if(l>m){
+//             m=l;
+//         }
+      
+// i=i+1;
+//     }
+
+//     while(i<n);
+//      printf("Largest number is %d",m);
+
+//     return 0;
+     
+// }
+
+// int main(){
+//     int countm=0,gender,f,countf=0;
+//     printf("Enteries in office for Male is 1 and female is 2 \n");
+//     do{
+//         scanf("%d",&gender);
+//         if(gender==1){
+//             countm=countm+1;
+//         }
+//         else if(gender==2){
+//             countf=countf+1;
+//         }
+//         else if(gender==3){
+//             break;
+//         }
+//         else{
+//             printf("invalid input \n");
+//         }
+        
+//     }
+//     while(0<1);
+//      printf("count of Male is %d: \n",countm );
+//      printf("count of Female is %d: ",countf);
+// }
 
 #include <stdio.h>
-
 int main(){
     int i=0;
     do{
-        i=i+1;
-        if(i%2==1){
+        if(i%2==0){
             printf("%d \n",i);
-        
         }
-
+        i=i+1;
     }
-    while(i<15);
+    while(i<10);
     return 0;
-}
-
-int main(){
-    int i=0,n,m,l;
-
-    printf("Enter the number n \n");
-    scanf("%d",&n);
-    printf("Enter the numbers \n");
-    scanf("%d",&m);
-    
-    do{
-        scanf("%d",&l);
-        if(l>m){
-            m=l;
-        }
-      
-i=i+1;
-    }
-
-    while(i<n);
-     printf("Largest number is %d",m);
-
-    return 0;
-     
-}
-
-int main(){
-    int countm=0,gender,f,countf=0;
-    printf("Enteries in office for Male is 1 and female is 2 \n");
-    do{
-        scanf("%d",&gender);
-        if(gender==1){
-            countm=countm+1;
-        }
-        else if(gender==2){
-            countf=countf+1;
-        }
-        else if(gender==3){
-            break;
-        }
-        else{
-            printf("invalid input \n");
-        }
-        
-    }
-    while(0<1);
-     printf("count of Male is %d: \n",countm );
-     printf("count of Female is %d: ",countf);
 }
